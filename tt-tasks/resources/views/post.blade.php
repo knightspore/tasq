cc669587514d64ee4fa952308cb82f429103a184
Ciaran <cslemon96@gmail.com>
2020-03-18 12:24:10 +0200

    added first badges to card posts and neatened up sass

--- tt-tasks/resources/views/post.blade.php
+++ tt-tasks/resources/views/post.blade.php
@@ -9,23 +9,34 @@
 
             <div class="card card-outline-primary mb-2 shadow">
                 <div class="card-header">
+                    <div>
+                    <h5 class="card-title"><span class="badge p-2">{{ $post->priority }}</span> • {{ $post->task }}</h5>
+                    </div>
+                    <div> 
                     @if (($post->progress) == "Complete")
-                    <h6 class="badge text-light badge-success text-success mb-1">{{ $post->progress }}</h6>
+                    <h6 class="badge text-light badge-success text-success mb-1">{{ $post->progress }}</h6> <h6 class="badge badge-info">{{ $post->site }}</h6>
                     @elseif (($post->progress) == "WIP")
-                    <h6 class="badge text-light badge-warning text-warning mb-1">{{ $post->progress }}</h6>
+                    <h6 class="badge text-light badge-warning text-warning mb-1">{{ $post->progress }}</h6> <h6 class="badge badge-info">{{ $post->site }}</h6>
                     @else
-                    <h6 class="badge text-light badge-secondary text-secondary mb-1">Not picked up</h6>
+                    <h6 class="badge text-light badge-secondary text-secondary mb-1">Not picked up</h6> <h6 class="badge badge-info">{{ $post->site }}</h6>
                     @endif
-                    <h5 class="card-title">{{ $post->priority }} | {{ $post->task }}</h5>
+                    </div>
                 </div>
 
                 <div class="card-body">
-                    <h6 class="card-subtitle mb-2">{{ $post->site }}</h6>
                     <h6 class="card-subtitle mb-2">{{ $post->type }} - {{ $post->points }}</h6>
                     @isset($post->user)
+                    <hr>
                     <h6 class="card-subtitle mb-2 text-muted">{{ $post->user }} is working on this post</h6>
                     @endisset
                     @empty($post->user)
+                    @endempty
+
+                    @isset($post->comment)
+                    <hr>
+                    <h6 class="card-subtitle text-muted">{{ $post->comment }}</h6>
+                    @endisset
+                    @empty($post->comment)
                     @endempty
                 </div>
 
