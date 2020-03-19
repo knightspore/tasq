6bc3d7881a70a70b7db24a6e778949f79dce559e
Ciaran <cslemon96@gmail.com>
2020-03-19 12:50:31 +0200

    added some functionality to the welcome screen

--- tt-tasks/resources/views/welcome.blade.php
+++ tt-tasks/resources/views/welcome.blade.php
@@ -6,9 +6,10 @@
 
     <!--Welcome Text-->
 
-	<div class="flex-center position-ref text-center">            
-            <div><h1 class=" mt-5">Welcome to Travel Tractions Task Manager</h1></div>
-            <button class="btn btn-secondary text-light"><a href="/login">Sign in</a></button>
+	<div class="flex-center position-ref text-center d-grid">            
+            <div><h1 class=" m-5">Welcome to Travel Tractions Task Manager</h1></div>
+            <div><img src="https://source.unsplash.com/1600x900/?south-africa" class="img-fluid w-50 mb-5 p-2 rounded" alt="Randomly Generated Inspirational Image"></div>
+            <button class="btn btn-primary"><a class="text-light" href="/login">Sign in</a></button>
     </div>
 
 @endsection
