4c821853f9d4af151b4ebef5012844e73e74ee9c
Ciaran <cslemon96@gmail.com>
2020-04-24 21:35:20 +0200

    added link to username in usercard blade

--- resources/views/components/usercard.blade.php
+++ resources/views/components/usercard.blade.php
@@ -5,7 +5,7 @@
         <div class="card-body border-primary">
         <img class="card-img-top rounded-circle img-thumbnail mb-3" src="{{ $user->avatar }}" alt="{{ $user->name }}">
 
-            <h4 class="card-title">{{ $user->name }}</h4>
+            <h4 class="card-title"><a href="/{{ $user->id }}">{{ $user->name }}</a></h4>
             <h5 class="text-success">{{ $user->role }}</h5>
             <p class="card-text"><small class="text-muted">Slack | <a href="mailto:{{ $user->email }}">Email</a> |
                     <a href="{{ $client->users->getUser($user->email)->gid }}">Asana</a> </small></p>
