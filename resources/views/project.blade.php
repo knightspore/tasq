8537718d3d152d3dc9a7d59dd251fc9178971edb
knightspore <cslemon96@gmail.com>
2020-04-27 00:07:17 +0200

    added edit project button

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -11,32 +11,38 @@
     <div class="row mt-2">
         <div class="col-md-5 py-3 text-center">
         <div class="mx-auto">
-        <img src="https://scontent.fcpt1-1.fna.fbcdn.net/v/t1.0-9/52351556_1102226693290301_5029265171558694912_n.png?_nc_cat=106&_nc_sid=85a577&_nc_oc=AQlJ1ZTnLdwLVhcMhR-vtQiBPlUSGSC94jB0ZRjoHb3F8cK2AfKb7UU8jKcFzNPOxdw&_nc_ht=scontent.fcpt1-1.fna&oh=02750184f02d8fefde2ed82e9a0bd19e&oe=5EC01C5F" alt="{{ $thisproject->name }} Logo" class="responsive-image w-75 mx-auto d-block rounded-circle mb-4 shadow-sm img-thumbnail">
+        <img src="https://scontent.fcpt1-1.fna.fbcdn.net/v/t1.0-9/52351556_1102226693290301_5029265171558694912_n.png?_nc_cat=106&_nc_sid=85a577&_nc_oc=AQlJ1ZTnLdwLVhcMhR-vtQiBPlUSGSC94jB0ZRjoHb3F8cK2AfKb7UU8jKcFzNPOxdw&_nc_ht=scontent.fcpt1-1.fna&oh=02750184f02d8fefde2ed82e9a0bd19e&oe=5EC01C5F" alt="{{ $thisproject->name }} Logo" class="responsive-image w-25 mx-auto d-block rounded-circle mb-4 shadow-sm img-thumbnail">
         </div>
         
         <h1><span class="text-success mb-5">{{ $thisproject->name }}</span></h1>
         
         <h4><a href="https://{{ $thisproject->site }}" class="text-muted" target="_blank">{{ $thisproject->site }}</a></h4>
         <hr>
-        <div class="text-right">
-        <h4><a href="{{ $thisproject->sop }}" class="text-dark">🔗 SOP</a></h4>
+        <div class="text-center">
         <h4>Account Manager: <span class="text-success">{{ $thisproject->accountmgr }}</span></h4>
+        <h4>Niche: <span class="text-success">{{ $thisproject->niche }}</span></h4>
         @if($thisproject->client == 1)
         <h4>Client Project</h4>
         @else
         <h4>Internal Project</h4>
         @endif
+        <h4><a href="{{ $thisproject->sop }}" class="text-dark">🔗 SOP</a></h4>
+        @if (Auth::user()->level >= 5)
+        <h6><a href="/project/{{ $thisproject->id }}/edit" class="text-small text-dark">Edit Project</a></h6>
+        @endif
         </div>
             
         </div>
         <div class="col-md-7 py-3">
+        <!-- About Project -->
+            <h2 class="pb-3">🧰 Project Info</h2>
         <!--Assigned Tasks-->
-            <h2 class="pb-3">🙌🏽 In Progress</h2>
+            <h2 class="pb-3">🙌🏽 Tasks</h2>
             @foreach($tasks as $task)
+            @if($task->site == $thisproject->site)
             @include('components.minitask')
+            @endif
             @endforeach
-        <!--Completed Tasks-->
-            <h2 class="pb-3">🥚 New Tasks</h2>
         </div>
     </div>
 </div>
